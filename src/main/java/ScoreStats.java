0cb27d162589de9370434c149adb85a3a9a0fe44
mrfawcett <pfawcett@fairmontschools.com>
2023-10-17 13:02:16 -0700

    Creating skeleton code

---
+++ src/main/java/ScoreStats.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class ScoreStats {
+    private ArrayList<ScoreInfo> scoreList;
+
+    /* COMPLETE THIS METHOD
+     * Records a score in the database, keeping the database in increasing score order. If no other ScoreInfo object represents score, a new ScoreInfo object representing score is added to the database; otherwise, the frequency in the ScoreInfo object representing score is incremented.
+     * @param score a score to be recorded in the list
+     * @return true if a new ScoreInfo object representing score was added to the list; false otherwise
+     */
+    public boolean record(int score) {
+        // Insert code below
+
+        return false;
+    }
+
+    /* COMPLETE THIS METHOD
+     * 
+     */
+    public void recordScores(int[] stuScores) {
+        // Insesrt code below
+
+
+    }
+}
